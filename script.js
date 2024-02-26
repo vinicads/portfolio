55f2b16eb90aa41f3473c0d78610059fdba71ae4
Vinicius Augusto <vinicim.augusto@outlook.com>
2024-02-26 10:28:46 -0300

    new changges

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ let navbar = document.getElementById('navbar')
 let lastScrollTop = 0;
 
 window.addEventListener('scroll', () => {
-   
-
+    
+    //parallax
     let value = window.scrollY;
-    let windowHeight = window.screen.height;
     text.style.marginTop = value * 2.5 + 'px';
     baixoFundo.style.marginTop = value * -1,5 + 'px'
 
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    //navbar
+    let windowWidth = window.screen.width;
+
+    if (windowWidth > 768){
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
     
     if (currentScroll < lastScrollTop) {
         navbar.classList.add('animate-out');
@@ -29,6 +32,11 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('animate-in');
         navbar.classList.add('animate-out');
     }
+    }
+});
+
+window.addEventListener('load', () => {
+    countTo();
 });
 
 function handleAnimationEnd(event) {
@@ -93,4 +101,72 @@ function typeWriter(index) {
             });
         }, 1000);
     });
+}
+
+function countTo(){
+    var countNumbers = document.querySelectorAll('.number');
+
+    countNumbers.forEach(element => {
+        let start = 0;
+        let end = parseInt(element.getAttribute('data-value'));
+        var timeWait = 500;
+        let duration = Math.floor(timeWait / end);
+        let counter = () => {
+            start += 1;
+            element.textContent = start;
+            duration = (timeWait / (end - start));
+            clearInterval(interval);
+            if (start == end){
+                return;
+            }else{
+                interval = setInterval(counter, duration)
+            }
+
+        };
+
+        var interval = setInterval(counter, duration);
+    });
+}
+
+function alternateContent(index){
+    let skills = document.getElementById('skills');
+    let education = document.getElementById('education');
+    let skillsMenu = document.getElementById('skillsMenu');
+    let educationMenu = document.getElementById('educationMenu');
+    if (index == 1){
+        skillsMenu.classList.add('active');
+        educationMenu.classList.remove('active');
+        if (skills.classList.contains('in')){
+            return;
+        }else{
+            education.classList.remove('in');
+            education.classList.add('out');
+            var change = () => {
+                education.style.display = 'none';
+                skills.style.display = 'grid';
+                skills.classList.remove('out');
+                skills.classList.add('in');
+                education.removeEventListener('animationend', change)
+            }
+            education.addEventListener('animationend', change);
+            
+        }
+    }else{
+        educationMenu.classList.add('active');
+        skillsMenu.classList.remove('active');
+        if (education.classList.contains('in')){
+            return;
+        }else{
+            skills.classList.remove('in');
+            skills.classList.add('out');
+            var change = () => {
+                skills.style.display = 'none';
+                education.style.display = 'grid';
+                education.classList.remove('out');
+                education.classList.add('in');
+                skills.removeEventListener('animationend', change)
+            }
+            skills.addEventListener('animationend', change);
+        }
+    }
 }
